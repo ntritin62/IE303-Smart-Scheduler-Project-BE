46e7a0bdbe888c94bc8c42ac88dda43881cac515
ntritin62 <21522677@gm.uit.edu.vn>
2024-05-28 14:45:11 +0700

    add async sendEmail

--- src/main/java/com/example/task_calendar/service/email/EmailServiceImpl.java
+++ src/main/java/com/example/task_calendar/service/email/EmailServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +29,7 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     CalendarService calendarService;
 
+    @Async
     public void sendEmail(String email, String subject, String body){
         System.out.println(123);
         SimpleMailMessage mailMsg = new SimpleMailMessage();
